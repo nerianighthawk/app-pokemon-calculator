795ffa4f11531ca976b3dcf9a7b853edf7e3e3ac
nerianighthawk <nerianighthawk@hiroyaMAC.local>
2018-06-13 20:47:33 +0900

    edit .gitignore

--- server/venv/src/controller/PokemonController.py
+++ server/venv/src/controller/PokemonController.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response
 from playhouse.postgres_ext import PostgresqlExtDatabase
 import sys
 
 sys.path.append('..')
-import model.PokemonModel
+from model.pokemon import Pokemon
 
-api = Flask(__name__)
-
-
-@api.route('/', methods=['GET'])
-def hello():
-    return 'hello:)'
+pokemonApi = Blueprint('pokemon', __name__)
 
 
-@api.route('/getPokemon/<string:pokemonNo>', methods=['GET'])
+@pokemonApi.route('/getPokemon/<string:pokemonNo>', methods=['GET'])
 def get_user(pokemonNo):
     try:
-        pokemon = model.PokemonModel.Pokemon.get(model.PokemonModel.Pokemon.pokemonNo == pokemonNo)
-    except model.PokemonModel.Pokemon.DoesNotExist:
+        pokemon = Pokemon.get(Pokemon.pokemonNo == pokemonNo)
+    except Pokemon.DoesNotExist:
         abort(404)
 
     result = {
@@ -36,12 +31,3 @@ def get_user(pokemonNo):
     return make_response(jsonify(result))
     # Unicodeにしたくない場合は↓
     # return make_response(json.dumps(result, ensure_ascii=False))
-
-
-@api.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-if __name__ == '__main__':
-    api.run(host='127.0.0.1', port=3000)
